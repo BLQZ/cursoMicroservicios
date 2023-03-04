38a450587d24c9cdb3edf6c60c4bc5266b6f3ede
BLQZ <alejandro22blazquez06@gmail.com>
2023-03-04 15:34:50 +0100

    push

--- springboot-servicio-productos/src/main/java/com/formacion/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacion/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,11 @@
 package com.formacion.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +17,45 @@ import com.formacion.springboot.app.productos.models.service.IProductoService;
 public class ProductoController {
 	
 	@Autowired
+	Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@Autowired
 	private IProductoService productoService;
 	
 	//métodos handler
 	@GetMapping("/listar")
 	public List<Producto> listar() {
-		return productoService.findAll();
+//		return productoService.findAll();
+		return productoService.findAll().stream()
+				.map(p -> {
+					p.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+//					p.setPort(port);
+					return p;
+				}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
+		
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		
 //		boolean ok = false;
 //		
 //		if(!ok) {
 //			throw new RuntimeException("No se pudo cargar el producto!");
 //		}
+		
+//		try {
+//			Thread.sleep(2000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		return productoService.findById(id);
 	}
 
