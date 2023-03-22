8b0d580f44fce81b99f7868b125073909de09e65
BLQZ <alejandro22blazquez06@gmail.com>
2023-03-22 17:03:01 +0100

    posgressql

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES('andres', '$2a$10$RKOYgh.xogZ.iI1.UYbNBuYlkjxwFxGri63GTMUvjSMUjdUtrSDNa', 1, 'Andres', 'Medina', 'andres.medina@oficina.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES('vanessa', '$2a$10$bYU5gCs8O1PEHPQtp9SABObnQDPOQL/Y.iwJyrdkVdKk3IviKeyGi', 1, 'Vanessa', 'Zambrano', 'vanessa@test.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES('andres', '$2a$10$RKOYgh.xogZ.iI1.UYbNBuYlkjxwFxGri63GTMUvjSMUjdUtrSDNa', true, 'Andres', 'Medina', 'andres.medina@oficina.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES('admin', '$2a$10$bYU5gCs8O1PEHPQtp9SABObnQDPOQL/Y.iwJyrdkVdKk3IviKeyGi', true, 'Administrador', 'Administrador', 'admin@test.com')
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES ('ROLE_USER')
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN')
 
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1)
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2)
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1)
